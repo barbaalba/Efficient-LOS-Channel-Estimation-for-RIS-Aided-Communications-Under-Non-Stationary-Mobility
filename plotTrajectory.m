780b1012e9752a071de98f76ab832b5e06ddd844
barbaalba <39873583+barbaalba@users.noreply.github.com>
2022-10-19 23:26:41 +0200

    Update plotTrajectory.m
    Added two different configuration for plotting the user trajectory

--- plotTrajectory.m
+++ plotTrajectory.m
@@ -1,4 +1,4 @@
-function plotTrajectory(x_t,y_t,Az)
+function plotTrajectory(x_t,y_t,Az,conf)
 K = size(x_t,1);
 T = size(x_t,2);
 for k = 1:K
@@ -8,17 +8,20 @@ for k = 1:K
     %tiledlayout(2,1);
     subplot(1,2,1);set(gca, 'ydir', 'reverse');xlabel('y');ylabel('x');
     grid on; xlim([-25,25]); ylim([-25,25]); hold on;
-    subplot(1,2,2);xlabel('time');ylabel('azimuth');grid on;ylim([-80,80]);
-    hold on;
-    for t = 1:T
+    subplot(1,2,2);xlabel('time');ylabel('azimuth');grid on;ylim([-90,90]);
+    xlim([1,T]);hold on;
+    for t = 2:T
     %nexttile(1);
     subplot(1,2,1);
-    plot(y_t(k,1:t),x_t(k,1:t),'Color','b');
+    if strcmp(conf,'discrete')
+        hold off;
+        plot(y_t(k,t-1:t),x_t(k,t-1:t),'Color','b'); set(gca, 'ydir', 'reverse');xlabel('y');ylabel('x');xlim([-25,25]); ylim([-25,25]);grid on;
+    else
+        plot(y_t(k,1:t),x_t(k,1:t),'Color','b');
+    end
     %nexttile(2);
     subplot(1,2,2);
     plot(rad2deg(Az(k,1:t)),'r');
     pause(0.1);
     end
 end
-
-
