ef501bdec96483eeadd1382bc27ab21d33109613
barbaalba <39873583+barbaalba@users.noreply.github.com>
2022-10-21 13:39:35 +0200

    Update randomwalk.m
    Removed some lines

--- randomwalk.m
+++ randomwalk.m
@@ -1,6 +1,4 @@
 function [x_t,y_t] = randomwalk(NumUE,RandomLength,Xmax,Ymax)
-%N = 1000; % Length of the x-axis, also known as the length of the random walks.
-%M = 2; % The amount of random walks.
 x_t = zeros(NumUE,RandomLength);
 y_t = zeros(NumUE,RandomLength);
 for m=1:NumUE
