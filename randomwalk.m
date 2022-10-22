170854cba37cdd8c2e646a068f401d67e4d43623
barbaalba <39873583+barbaalba@users.noreply.github.com>
2022-10-22 21:26:42 +0200

    Update randomwalk.m
    Increased the resolution of the walk

--- randomwalk.m
+++ randomwalk.m
@@ -4,17 +4,17 @@ y_t = zeros(NumUE,RandomLength);
 for m=1:NumUE
   for n = 1:RandomLength % Looping all values of N into x_t(n).
       if x_t(m,n) < Xmax && x_t(m,n)>-Xmax
-          A = sign(randn); % Generates either +1/-1 depending on the SIGN of RAND.
+          A = 0.25*sign(randn); % Generates either +1/-1 depending on the SIGN of RAND.
           x_t(m,n+1) = x_t(m,n) + A;
       else
-          A = -sign(x_t(m,n));
+          A = -0.25 * sign(x_t(m,n));
           x_t(m,n+1) = x_t(m,n) + A;
       end
       if y_t(m,n) < Ymax && y_t(m,n) > -Ymax
-          A = sign(randn); % Generates either +1/-1 depending on the SIGN of RAND.
+          A = 0.25*sign(randn); % Generates either +1/-1 depending on the SIGN of RAND.
           y_t(m,n+1) = y_t(m,n) + A;
       else
-          A = -sign(y_t(m,n));
+          A = -0.25*sign(y_t(m,n));
           y_t(m,n+1) = y_t(m,n) + A;
       end
   end
