a819a712fdfb7a9828b9cb3885814df024e83cdf
barbaalba <39873583+barbaalba@users.noreply.github.com>
2022-10-27 21:25:15 +0200

    Update randomwalk.m
    Added the speed as an additional variable in random walk

--- randomwalk.m
+++ randomwalk.m
@@ -1,20 +1,20 @@
-function [x_t,y_t] = randomwalk(NumUE,RandomLength,Xmax,Ymax)
+function [x_t,y_t] = randomwalk(NumUE,RandomLength,Xmax,Ymax,SP)
 x_t = zeros(NumUE,RandomLength);
 y_t = zeros(NumUE,RandomLength);
 for m=1:NumUE
   for n = 1:RandomLength % Looping all values of N into x_t(n).
       if x_t(m,n) < Xmax && x_t(m,n)>-Xmax
-          A = 0.25*sign(randn); % Generates either +1/-1 depending on the SIGN of RAND.
+          A = SP*sign(randn); % Generates either +1/-1 depending on the SIGN of RAND.
           x_t(m,n+1) = x_t(m,n) + A;
       else
-          A = -0.25 * sign(x_t(m,n));
+          A = -SP * sign(x_t(m,n));
           x_t(m,n+1) = x_t(m,n) + A;
       end
       if y_t(m,n) < Ymax && y_t(m,n) > -Ymax
-          A = 0.25*sign(randn); % Generates either +1/-1 depending on the SIGN of RAND.
+          A = SP*sign(randn); % Generates either +1/-1 depending on the SIGN of RAND.
           y_t(m,n+1) = y_t(m,n) + A;
       else
-          A = -0.25*sign(y_t(m,n));
+          A = -SP*sign(y_t(m,n));
           y_t(m,n+1) = y_t(m,n) + A;
       end
   end
