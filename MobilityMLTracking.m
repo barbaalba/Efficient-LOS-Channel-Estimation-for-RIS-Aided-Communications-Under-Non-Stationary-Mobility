a33bae9aa26c5c0cf25e385f4fb99949783d8ddb
Mehdino <mehdi.haghshenas93@yahoo.com>
2022-11-21 09:58:41 +0100

    Use 6 pilot to re-estimate the channel
    It contains the R variable that collects the last iteration results so to plot how outdated result affect the SE

--- MobilityMLTracking.m
+++ MobilityMLTracking.m
@@ -1,7 +1,7 @@
 close all; clc; clear; 
 freq = 28e9; % Central frequency
 lambda = physconst('LightSpeed') / freq; % Wavelength
-rng(1);
+rng(4);
 % Mobility Config 
 Speed = 0.1; numUE = 1; RWL = 1000;
 Xmax = 5; Ymax = 5; z_t = repelem(1.5,numUE,RWL+1); randchan = false;
@@ -15,7 +15,7 @@ else
 end
 
 % Tracking config
-Prep = 5; % period of channel estimation
+Prep = 50; % period of channel estimation
 SRes = 100; % search resolution
 %UPA Element configuration
 M_H = 8; M_V = 8; M = M_H*M_V;
@@ -38,7 +38,7 @@ Dh = diag(h);
 Dh_angles = diag(h./abs(h)); 
 
 nbrOfAngleRealizations = 1000;
-nbrOfNoiseRealizations = 1;
+nbrOfNoiseRealizations = 100;
 
 
 %Save the rates achieved at different iterations of the algorithm
@@ -57,13 +57,16 @@ for i = 1:length(ElAngles)
     beamresponses(:,(i-1)*length(AzAngles)+1: i*length(AzAngles)) = UPA_Evaluate(lambda,M_V,M_H,AzAngles,repelem(ElAngles(i),1,length(AzAngles)),elementspacing,elementspacing);
 end
 
-%plot the Configured angles grid
-% for i = 1:length(beamAngles)
-%     figure(1);
-%     grid on;
-%     plot(rad2deg(beamAngles(i,2)),rad2deg(beamAngles(i,1)),'*','MarkerSize',10,'Color','b');
-%     hold on;
-% end
+% Define a fine grid of angle directions to analyze when searching for angle of arrival
+varphi_range = linspace(-pi/2,pi/2,SRes);
+theta_range = linspace(-pi/2,pi/2,SRes);
+a_varphi_range = zeros(M,SRes,SRes); % [M,Azimuth,Elevation]
+% obtain the array response vectors for all azimuth-elevation pairs
+parfor i = 1:SRes
+    a_varphi_range(:,:,i) = ...
+    UPA_Evaluate(lambda,M_V,M_H,varphi_range,repelem(theta_range(i),1,SRes),elementspacing,elementspacing);
+end
+
 idx = zeros(nbrOfAngleRealizations,nbrOfNoiseRealizations);
 for n1 = 1:nbrOfAngleRealizations
     disp(n1);
@@ -78,22 +81,11 @@ for n1 = 1:nbrOfAngleRealizations
     var_pow_d= 10;
     d = sqrt(var_pow_d/2) * (randn + 1i*randn); % CN(0,10)
 
-    % Define a fine grid of angle directions to analyze when searching for angle of arrival
-    varphi_range = linspace(-pi/2,pi/2,SRes);
-    theta_range = linspace(-pi/2,pi/2,SRes);
-    a_varphi_range = zeros(M,SRes,SRes); % [M,Azimuth,Elevation]
-
-    % obtain the array response vectors for all azimuth-elevation pairs
-    parfor i = 1:SRes
-        a_varphi_range(:,:,i) = ...
-            UPA_Evaluate(lambda,M_V,M_H,varphi_range,repelem(theta_range(i),1,SRes),elementspacing,elementspacing);
-    end
-
     %Compute the exact capacity for the system Eq. (3)
     capacity(n1) = log2(1+SNR_data*(sum(abs(Dh*g)) + abs(d))^2);
-    if mod(n1,Prep) == 1
-        for n2 = 1:nbrOfNoiseRealizations
-
+    
+    for n2 = 1:nbrOfNoiseRealizations
+        if mod(n1,Prep) == 1
             %Select which two RIS configurations from the grid of beams to start with
             utilize = false(M,1);
             if n1 == 1
@@ -110,7 +102,7 @@ for n1 = 1:nbrOfAngleRealizations
                     idx(n1,n2) = randi(M-M_H)+M_H;
                 end
                 utilize(idx(n1,n2)) = true;
-                Plim = 5;
+                Plim = 6;
             end
 
             %Define the initial transmission setup
@@ -200,24 +192,26 @@ for n1 = 1:nbrOfAngleRealizations
                     [~,bestInit1] = max(closestBeam);
                 end
             end
+        else
+            rate_proposed(itr,n1,n2) = log2(1+SNR_data*abs(exp(-1i*RISconfig).'*Dh*g + d)^2);
         end
-
-    else
-        rate_proposed(itr,n1,n2) = log2(1+SNR_data*abs(exp(-1i*RISconfig).'*Dh*g + d)^2);
-        capacity(n1) = log2(1+SNR_data*(sum(abs(Dh*g)) + abs(d))^2);
     end
 end
-
+Rred = mean(rate_proposed,3);
+R = zeros(1,RWL);
+R(1:Prep) = Rred(M-1,1:Prep);
+R(Prep+1:end) = Rred(Plim-1,Prep+1:end);
 set(groot,'defaultAxesTickLabelInterpreter','latex');  
 figure; 
-plot(capacity,'--','LineWidth',2); hold on;
-plot(rate_proposed(63,:),'LineWidth',2);
+plot(capacity,'-k','LineWidth',5); hold on;
+plot(R,'LineWidth',2);
 %xline(1:Prep:nbrOfAngleRealizations);
-plot(1:Prep:nbrOfAngleRealizations,rate_proposed(63,1:Prep:nbrOfAngleRealizations),'Marker','o','LineStyle','none','MarkerSize',10,'LineWidth',2);
-xticks(0:100:1000);
+plot(1:Prep:nbrOfAngleRealizations,R(1:Prep:nbrOfAngleRealizations),'Marker','o','LineStyle','none','MarkerSize',15,'LineWidth',4);
+xticks(0:100:1000); ylim([0,9.2]); grid on; box on;
 xticklabels({'0','20','40','60','80','100','120','140','160','180','200'})
-legend({'Perfect CSI','1 seconds','10 seconds','Update '},'Fontsize',20,'Interpreter','latex');
-
+legend({'Perfect CSI','1\textsuperscript{st} Policy (1 second)','2\textsuperscript{nd} Policy (10 seconds)','Update points'},'Fontsize',20,'Interpreter','latex');
+ylabel('Spectral Efficiency (bit/s/Hz)','Interpreter','latex');
+xlabel('Time (s)','Interpreter','latex');
 figure;
 hold on; box on; grid on;
 plot(2:M,mean(capacity)*ones(M-1,1),'r:','LineWidth',2)
@@ -225,7 +219,7 @@ plot(2:M,mean(mean(rate_proposed,3),2),'k-','LineWidth',2)
 plot(2:M,mean(mean(rate_LS,3),2),'b-.','LineWidth',2)
 ax = gca;
 xlabel('Number of pilot transmissions','Interpreter','latex');
-ylabel('Average rate (bits/s/Hz)','Interpreter','latex');
+ylabel('Average rate (bit/s/Hz)','Interpreter','latex');
 legend({'Perfect CSI','Proposed ML estimator','Least-squares estimator'},'Interpreter','latex','Location','SouthEast');
 set(gca,'fontsize',16);
 ylim([0 ceil(max(capacity))+0.5]);
