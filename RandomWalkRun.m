e925bf7687a16a681fd090532ddc358191c14055
Mehdino <mehdi.haghshenas93@yahoo.com>
2024-02-17 11:52:42 +0330

    Removed unnecessary codes

--- RandomWalkRun.m
+++ RandomWalkRun.m
@@ -19,12 +19,7 @@ plt = true; % To plot the trajectory
 pltconf = 'discrete'; % 'continous' or 'discrete'
 RIS_coor = [-Xmax,0,2]; % the RIS is located at the end of the room 
 z_t = repelem(1.5,numUE,RWL+1); % Z-coordinate of the user(does not change)
-% channel parameters (LOS mmWave) alpha + 10*beta*log10(d)
-alpha = 61.4; % path loss reference in 1 (m) distance
-beta = 1.46;
-plEval = @(d) alpha + 10*beta*log10(d);
-noisepow = -96; % [dBm]
-txpow = 0; % [dBm]
+
 %% Extract Azimuth-Elevation-Distance
 % Random walk
 [x_t,y_t] = randomwalk(numUE,RWL,Xmax,Ymax,Speed);
@@ -35,26 +30,5 @@ if plt
     plotTrajectory(x_t,y_t,azimuth,elevation,pltconf,Xmax,Ymax,RIS_coor);
 end
 % uncomment it to save the required data
-save('fast20.mat','x_t','y_t','azimuth','elevation','Cph'); 
-%% Direct Channel model 
-g_d = -130; % the NLOS direct channel gain
-h_d = sqrt(db2pow(g_d)) * (randn+1i*randn); % direct channel complex gain
-%% End-End channel 
-% channel between RIS and BS
-PLdB = plEval(d_BSRIS);
-RIS_AOA = pi/6; % Azimuth of arrival at RIS from BS
-RIS_EOA = 0; % Elevation of arrival to RIS from BS (They are in the same level)
-g = sqrt(db2pow(PLdB))*exp(1i*2*pi/lambda*d_BSRIS)*...
-    UPA_Evaluate(lambda,M_V,M_H,RIS_AOA,RIS_EOA,RISspacing,RISspacing);
-% channel between RIS and UE and the algorithm
-UEPL = plEval(d_t);
-disp(['End to End path loss is ' num2str(mean(UEPL,'all')+PLdB) '[dB]']);
-h = zeros(M_H*M_V,RWL+1,numUE);
-for k=1:numUE
-    h(:,:,k) = sqrt(db2pow(-UEPL(k,:))) .*UPA_Evaluate(lambda,M_V,M_H,azimuth(k,:),elevation(k,:),RISspacing,RISspacing);
-end
-
-% Enlarge figure to full screen.
-% set(gcf, 'Units', 'Normalized', 'Outerposition', [0, 0.05, 1, 0.95]);
-% axis square;
+%save('fast20.mat','x_t','y_t','azimuth','elevation','Cph'); 
 
